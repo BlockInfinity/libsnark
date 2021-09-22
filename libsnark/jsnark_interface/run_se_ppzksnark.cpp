09f16013f2c4ce9525b812e9e80c902d77a23af2
Christoph Michelbach <michelbach94@gmail.com>
2021-09-22 18:51:19 +0200

    Provide setup generation and proof generation separately.

--- libsnark/jsnark_interface/run_se_ppzksnark.cpp
+++ libsnark/jsnark_interface/run_se_ppzksnark.cpp
@@ -22,54 +22,92 @@ int main(int argc, char **argv) {
 	gadgetlib2::GadgetLibAdapter::resetVariableIndex();
 	ProtoboardPtr pb = gadgetlib2::Protoboard::create(gadgetlib2::R1P);
 
-	int operation = -1;
 	int inputStartIndex = 1;
 
 	// argv[1] specifies the operation. Valid values are:
 	// - setup
 	// - generate-proof
-	// - verify-proof
 	
 	// Read the circuit, evaluate, and translate constraints
 	CircuitReader reader(argv[1 + inputStartIndex], argv[2 + inputStartIndex], pb);
 	r1cs_constraint_system<FieldT> cs = get_constraint_system_from_gadgetlib2(*pb);
-	const r1cs_variable_assignment<FieldT> full_assignment =
-		get_variable_assignment_from_gadgetlib2(*pb);
-	cs.primary_input_size = reader.getNumInputs() + reader.getNumOutputs();
-	cs.auxiliary_input_size = full_assignment.size() - cs.num_inputs();
 
-	// extract primary and auxiliary input
-	const r1cs_primary_input<FieldT> primary_input(full_assignment.begin(),
-												   full_assignment.begin() + cs.num_inputs());
-	const r1cs_auxiliary_input<FieldT> auxiliary_input(
-													   full_assignment.begin() + cs.num_inputs(), full_assignment.end());
+	if(strcmp(argv[1], "setup") == 0) {
+		// Generate a key pair.
+		r1cs_se_ppzksnark_keypair<libff::alt_bn128_pp> keypair = r1cs_se_ppzksnark_generator<libff::alt_bn128_pp>(cs);
 
-	printElapsedTime("defined input", startTime);
-	// only print the circuit output values if both flags MONTGOMERY and BINARY outputs are off (see CMakeLists file)
-	// In the default case, these flags should be ON for faster performance.
+		// Save the key pair to the disk.
+		ofstream serializationFile1;
+		serializationFile1.open("pk");
+		serializationFile1 << keypair.pk;
+		serializationFile1.close();
 
-	//assert(cs.is_valid());
+		ofstream serializationFile2;
+		serializationFile2.open("vk");
+		serializationFile2 << keypair.vk;
+		serializationFile2.close();
 
-	// removed cs.is_valid() check due to a suspected (off by 1) issue in a newly added check in their method.
-	// A follow-up will be added.
-	if(!cs.is_satisfied(primary_input, auxiliary_input)){
-		cout << "The constraint system is not satisifed by the value assignment - Terminating." << endl;
-		return -1;
+		// Print the verification key.
+		std::cout << "PRINTING VERIFICATION KEY" << std::endl;
+		keypair.vk.H.print();
+		keypair.vk.G_alpha.print();
+		keypair.vk.H_beta.print();
+		keypair.vk.G_gamma.print();
+		keypair.vk.H_gamma.print();
+		for(std::size_t i = 0; i < keypair.vk.query.size(); ++i) {
+			keypair.vk.query[i].print();
+		}
+
+		return 0;
 	}
-	printElapsedTime("checked constraint system satisfaction", startTime);
 
-	r1cs_example<FieldT> example(cs, primary_input, auxiliary_input);
-	printElapsedTime("created example", startTime);
-	const bool test_serialization = false;
-	bool successBit = false;
+	if(strcmp(argv[1], "generate-proof") == 0) {
+		// Read the key pair from the disk.
+		r1cs_se_ppzksnark_keypair<libff::alt_bn128_pp> keypair = r1cs_se_ppzksnark_keypair<libff::alt_bn128_pp>();
+	
+		string s1;
+		string line1;
+		ifstream serializationFile1;
+		serializationFile1.open("pk");
+		if(!serializationFile1.is_open()) {
+			cout << "Problem occurred while opening a file." << endl;
+			return -1;
+		}
+		serializationFile1 >> keypair.pk;
+		serializationFile1.close();
+		
+		string s2;
+		string line2;
+		ifstream serializationFile2;
+		serializationFile2.open("vk");
+		if(!serializationFile2.is_open()) {
+			cout << "Problem occurred while opening a file." << endl;
+			return -1;
+		}
+		serializationFile2 >> keypair.vk;
+		serializationFile2.close();
 
-	successBit = libsnark::run_r1cs_se_ppzksnark<libff::default_ec_pp>(example, test_serialization, startTime, true);
-	printElapsedTime("ran example function", startTime);
+		// Generate the proof.
+		const r1cs_variable_assignment<FieldT> full_assignment =
+			get_variable_assignment_from_gadgetlib2(*pb);
+		const r1cs_primary_input<FieldT> primary_input(full_assignment.begin(), full_assignment.begin() + cs.num_inputs());
+		const r1cs_auxiliary_input<FieldT> auxiliary_input(full_assignment.begin() + cs.num_inputs(), full_assignment.end());
+		
+		r1cs_se_ppzksnark_proof<libff::alt_bn128_pp> proof = r1cs_se_ppzksnark_prover<libff::alt_bn128_pp>(keypair.pk, primary_input, auxiliary_input);
 
-	if(!successBit){
-		cout << "Problem occurred while running the ppzksnark algorithms .. " << endl;
-		return -1;
-	}	
-	return 0;
+		// Print the proof.
+		std::cout << "PRINTING PROOF COORDINATES" << std::endl;
+		proof.A.to_affine_coordinates();
+		proof.A.print_coordinates();
+		proof.B.to_affine_coordinates();
+		proof.B.print_coordinates();
+		proof.C.to_affine_coordinates();
+		proof.C.print_coordinates();
+
+		return 0;
+	}
+
+	cout << "Invalid operation selected." << endl;
+	return 2;
 }
 
